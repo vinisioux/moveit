e7cacb00e120f42b06f9fddc2ea0d874611155f3
ViniciusLeonadovky <vile.pb@gmail.com>
2021-02-28 19:17:37 -0300

    fix: prisma fields on database

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -9,7 +9,7 @@ export function Profile() {
   const { level } = useContext(ChallengesContext);
   const [session] = useSession();
 
-  console.log(session);
+  console.log('session ->', session);
 
   return (
     <Container>
